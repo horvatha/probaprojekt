046e75211014693dce70a2c4b98e063ece9ac6d8
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2017-10-17 19:10:46 +0200

    index.html with a for cycle

--- veletlen/views.py
+++ veletlen/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
+    context = {"idezetek": [
+     "Nem esik messze",
+     "Néma gyereknek az anyja se látja a fátol az erdőt.",
+     "Áfásszámlaigényét kérjük előre jelezze",
+     "A nyúlon túl.",
+    ]}
+    return render(request, "index.html", context)
